a3045dc2239bac77cbaab59623c148de2cff083c
twc829 <rainbow_twc@163.com>
2016-07-14 08:45:29 +0800

    Init CloudAlbum.java file

---
+++ CloudAlbum.java
@@ -0,0 +1,12 @@
+public class CloudAlbum{
+	String name;
+	
+	public static void main(String[] args){
+		CloudAlbum ca = new CloudAlbum("Sissy_Express");
+
+	}
+	public CloudAlbum(String name){
+		this.name = name;
+	}
+
+}
