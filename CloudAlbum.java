2f6c67bfcc7c418fc904f8d7d3d9c3c260143815
twc829 <rainbow_twc@163.com>
2016-08-06 09:11:32 +0800

    jenkins test

--- CloudAlbum.java
+++ CloudAlbum.java
@@ -1,12 +1,14 @@
 public class CloudAlbum{
 	String name;
 	String date;
-	
+		
 	public static void main(String[] args){
 		CloudAlbum ca = new CloudAlbum("Sissy_Express");
 	}
 	public CloudAlbum(String name){
 		this.name = name;
 	}
-
+	public CloudAlbum(){
+		
+	}
 }
